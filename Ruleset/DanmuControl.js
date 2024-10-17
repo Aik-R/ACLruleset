7145f36f23ddabce0868eb8092dd244776d56b3d
Aisaka_RJ <72722916+Aik-R@users.noreply.github.com>
2024-10-17 23:36:24 +0800

    Update DanmuControl.js

--- Ruleset/DanmuControl.js
+++ Ruleset/DanmuControl.js
@@ -1,26 +1,16 @@
 !function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports["utils/keymap-danmu-toggle"]=t():e["utils/keymap-danmu-toggle"]=t()}(globalThis,(()=>(()=>{"use strict";var e={d:(t,a)=>{for(var o in a)e.o(a,o)&&!e.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:a[o]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t)},t={};e.d(t,{plugin:()=>a});const a={name:"keymap.actions.toggleDanmu",displayName:"快捷键扩展 - 弹幕控制",description:'在快捷键的动作列表里添加弹幕控制功能.',setup:e=>{let{addData:t}=e;
 
     const SELECTOR = {
-        dmTypeScroll: ".bpx-player-block-typeScroll",
-        dmTypeTop: ".bpx-player-block-typeTop",
-        dmTypeBottom: ".bpx-player-block-typeBottom",
-        dmTypeColor: ".bpx-player-block-typeColor",
-        dmTypeSpecial: ".bpx-player-block-typeSpecial",
+        dmTypeScroll: ".bpx-player-block-filter-type bpx-player-block-typeScroll",
+        dmTypeBottom: ".bpx-player-block-filter-type bpx-player-block-typeTopBottom",
+        dmTypeColor: ".bpx-player-block-filter-type bpx-player-block-typeColor",
+        dmTypeSpecial: ".bpx-player-block-filter-type bpx-player-block-typeSpecial",
     };
-
-    t("keymap.actions",(e=>{
-
-        e.danmuTop = {
-            displayName: "顶部弹幕",
-            run: async () => {
-                danmuType(SELECTOR.dmTypeTop);
-            }
-        };
-        
+    
         e.danmuBottom = {
-            displayName: "底部弹幕",
+            displayName: "固定弹幕",
             run: async () => {
-                danmuType(SELECTOR.dmTypeBottom);
+                danmuType(SELECTOR.typeTopBottom);
             }
         };
         
@@ -58,13 +48,11 @@
     }));
 
     t("keymap.presets",((e,t)=>{
-        e.danmuTop = 't';
-        e.danmuBottom = 'b';
+        e.typeTopBottom = 'b';
         e.danmuScroll = 's';
         e.danmuColor = 'c';
         e.danmuSpecial = ''; // No shortcut assigned
-        t.HTML5Player.danmuTop = '';
-        t.HTML5Player.danmuBottom = '';
+        t.HTML5Player.typeTopBottom = '';
         t.HTML5Player.danmuScroll = '';
         t.HTML5Player.danmuColor = '';
         t.HTML5Player.danmuSpecial = '';
